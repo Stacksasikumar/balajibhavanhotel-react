a6678ea616c73c2cab77dadc6c1acb6bf82d41d7
Stacksasikumar <developer1.sasikumar@gmail.com>
2022-12-26 21:13:56 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+// import About from './pages/About/About';
+// import Contact from './pages/Contact/Contact';
+// import Home from './pages/Home/Home';
+// import Services from './pages/Service/Services';
+// import Testimonial from './pages/Testimonial/Testimonial';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Router>
+    <Navbar/>
+    <Header/>
+    <main>
+      <Routes>
+        
+
+      </Routes>
+    </main>
+   </Router>
+
+  </>
   );
 }
+
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import Banner from "../banner.jpg";
+const Header = () => {
+  return (
+    <div>
+          
+           <h1>
+            <div style={{color:'blue', backgroundColor:"cadetblue"}}>
+              <label>Sri Balaji Bhavan</label> </div>     
+           {/* <img src={Banner} height="200px" alt="Sri Balaji Bhavan" width="100%"/> */}
+           </h1>
+  </div>
+  )
+}
+
+export default Header
